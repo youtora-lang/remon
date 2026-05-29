1fe09fde9d143be1cf6a8dea4cd7515115c4e3d5
youtora-lang <chiteru86youtora@gmail.com>
2026-05-29 18:41:14 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'remon-v1';
+const CACHE_NAME = 'remon-v2';
 const ASSETS = [
   './index.html',
   './manifest.json',
@@ -6,24 +6,32 @@ const ASSETS = [
   './icon-512.png'
 ];
 
-self.addEventListener('install', e => {
+self.addEventListener('install', function(e) {
   e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then(function(cache) { return cache.addAll(ASSETS); })
   );
   self.skipWaiting();
 });
 
-self.addEventListener('activate', e => {
+self.addEventListener('activate', function(e) {
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(k) { return k !== CACHE_NAME; })
+            .map(function(k) { return caches.delete(k); })
+      );
+    })
   );
   self.clients.claim();
 });
 
-self.addEventListener('fetch', e => {
+self.addEventListener('fetch', function(e) {
   e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request))
+    caches.match(e.request).then(function(cached) {
+      return fetch(e.request).then(function(fresh) {
+        var cache = caches.open(CACHE_NAME).then(function(c) { c.put(e.request, fresh.clone()); });
+        return fresh;
+      }).catch(function() { return cached; });
+    })
   );
 });
